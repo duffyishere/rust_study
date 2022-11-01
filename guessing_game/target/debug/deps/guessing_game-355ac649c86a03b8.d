ab4817c80539b8d310e9a45d7d825c7b0f7cd548
zzunho9 <jamlove4763@naver.com>
2022-11-02 01:08:48 +0900

    gitignore 적용

--- guessing_game/target/debug/deps/guessing_game-355ac649c86a03b8.d
+++
@@ -1,5 +0,0 @@
-/Users/zzun_ho9/sideProject/rust/guessing_game/target/debug/deps/guessing_game-355ac649c86a03b8: src/main.rs
-
-/Users/zzun_ho9/sideProject/rust/guessing_game/target/debug/deps/guessing_game-355ac649c86a03b8.d: src/main.rs
-
-src/main.rs:
--- guessing_game/target/debug/deps/guessing_game-57a7d5420987e087.d
+++
@@ -1,5 +0,0 @@
-/Users/zzun_ho9/sideProject/rust/guessing_game/target/debug/deps/guessing_game-57a7d5420987e087: src/main.rs
-
-/Users/zzun_ho9/sideProject/rust/guessing_game/target/debug/deps/guessing_game-57a7d5420987e087.d: src/main.rs
-
-src/main.rs:

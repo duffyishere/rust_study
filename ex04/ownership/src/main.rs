67a17086aa9515d987f21a620e199c42fd62fff3
zzunho9 <jamlove4763@naver.com>
2022-11-07 01:09:56 +0900

    ex04-03

--- ex04/ownership/src/main.rs
+++ ex04/ownership/src/main.rs
@@ -1,16 +1,22 @@
 fn main() {
-    let s = String::from("Hello");
-    takes_ownership(s);
-    // println!("{}", s);
+    // let s = String::from("Hello");
+    // takes_ownership(s);
+    // // println!("{}", s);
+    //
+    // let x: u32 = 20;
+    // make_copy(x);
+    // println!("x: {}", x);
+    //
+    // let s1 = String::from("hello");
+    // let (s2, length) = get_size(s1);
+    //
+    // println!("{}'s length: {}", s2, length)
 
-    let x: u32 = 20;
-    make_copy(x);
-    println!("x: {}", x);
+    let mut str = String::from("Hello World!");
+    let word = first_world(& str);
+    str.clear();
 
-    let s1 = String::from("hello");
-    let (s2, length) = get_size(s1);
-
-    println!("{}'s length: {}", s2, length)
+    println!("{}", word);
 }
 
 fn takes_ownership(some_string: String) {
@@ -24,4 +30,16 @@ fn make_copy(x: u32) {
 fn get_size(s1: String) -> (String, usize) {
     let length = s1.len();
     (s1, length)
+}
+
+fn first_world(str: &str) -> &str {
+    let byte = str.as_bytes();
+
+    for (i, &item) in byte.iter().enumerate() {
+        if item == b' ' {
+            return &str[.. i];
+        }
+    }
+
+    &str[..]
 }
